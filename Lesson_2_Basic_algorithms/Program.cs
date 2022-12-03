d22534c119be21edad58f8e6fa162edb08074f80
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-03 10:17:07 +0300

    Homework

--- Lesson_2_Basic_algorithms/Program.cs
+++ Lesson_2_Basic_algorithms/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. генерация случайного числа из диапозона 
+// 2. получить 1 цифру
+// 3. 2 цифру
+// 4. if
+// 5.else
+
+int N = new Random().Next(10, 99);
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
