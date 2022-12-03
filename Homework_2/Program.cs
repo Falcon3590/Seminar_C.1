692b44356422b98a6a420fa1af31da80b86c68a8
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-03 10:25:53 +0300

    Homework_2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = new Random().Next(1, 65536);
+
+if (N <= 99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    int ThirdNumber = (N / 100) % 10;
+    Console.Write($"{ThirdNumber} является третей цифрой от числа {N}");
+}
