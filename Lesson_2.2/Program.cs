423a6053d469b95f83091fb8c635c67803e2f671
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-03 10:19:10 +0300

    Lesson_2.2

---
+++ Lesson_2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("введите первое число: ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+if (firstDigit > secondDigit)
+{
+    int temp = firstDigit % secondDigit;
+    if (temp == 0)
+    {
+        Console.WriteLine("кратно");
+    }
+    else
+    {
+        Console.WriteLine("не кратно, остаток " + temp);
+    }
+}
+else
+{
+    int temp = secondDigit % firstDigit;
+    if (temp == 0)
+    {
+        Console.WriteLine("кратно");
+    }
+    else
+    {
+        Console.WriteLine("не кратно, остаток " + temp);
+    }
+}
